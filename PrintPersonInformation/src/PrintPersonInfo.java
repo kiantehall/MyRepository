b8c0c5d81e4608e863d7a9aa5749f3e579fe78fa
Kiante Hall <kiante@Kiantes-MBP.attlocal.net>
2024-08-24 20:00:11 -0400

    first commit

---
+++ PrintPersonInformation/src/PrintPersonInfo.java
@@ -0,0 +1,19 @@
+
+public class PrintPersonInfo {
+	    public static void main(String[] args) {
+	        // Declare variables and assign values
+	        String firstName = "Jake";
+	        String lastName = "Farm";
+	        String streetAddress = "339 Orange Ct";
+	        String city = "Clemson";
+	        String zipCode = "29229";
+
+	        // Print each variable on a new line
+	        System.out.println("First Name: " + firstName);
+	        System.out.println("Last Name: " + lastName);
+	        System.out.println("Street Address: " + streetAddress);
+	        System.out.println("City: " + city);
+	        System.out.println("Zip Code: " + zipCode);
+	    }
+	}
+
