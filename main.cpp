7f1bbe08e67b23f853ba1dd1f8331f1c706d880c
kiantehall <kiantehall@gmail.com>
2025-04-20 21:18:19 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string firstName = "Clark";
+    string lastName = "Kent";
+    string streetAddress = "1938 Sullivan Place";
+    string city = "Metropolis";
+    string zipCode = "33866";
+
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
